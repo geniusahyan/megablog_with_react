c26ce5853d0515e4069768007d5fdd521f9fff64
Aslam Ahyan <aslamalam078523@gmail.com>
2024-01-22 01:22:08 +0530

    header footer app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
-import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { useEffect, useState } from 'react';
+import {login, logout} from './store/authSlice'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
 
-  return (
-    <>
-        <h1>megablog</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrectUser()
+    .then((userData)=>{
+      if (userData) {
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+
+  }, [])
+  
+  return !loading ? (
+    <div className="min-h-screen text-white flex flex-wrap text-center bg-[#0c1d30] ">
+      <div className="w-full block">
+        <Header />
+        <main>
+          LIST: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
